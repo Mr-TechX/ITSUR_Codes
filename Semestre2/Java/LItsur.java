f0063dd9bab50d9a488b48de47d09220124584c3
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2023-01-27 09:38:56 -0600

    Update and rename Main1.java to LItsur.java

---
+++ Semestre2/Java/LItsur.java
@@ -0,0 +1,14 @@
+public class LItsur {
+	public static void main(String[] args) {
+		System.out.println("      __d888888b__");
+		System.out.println("   _d888P  __  Y888b_");
+		System.out.println("_d888P  _d8888b_  Y888b_");
+		System.out.println("888Y  d888P   Y88b   YP");
+		System.out.println("888    Y88888b_ YP");
+		System.out.println("888  Y8b_  Y888Y_");
+		System.out.println("888b_  Y88b_ d888P _b");
+		System.out.println(" Y888b_  Y8888P  _d88P");
+		System.out.println("    Y888b_    _d888P");
+		System.out.println("       Y88888888P");
+	}
+}
