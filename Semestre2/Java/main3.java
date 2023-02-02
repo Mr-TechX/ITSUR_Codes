e231a8736a01125328740b4121442680ab797dc1
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2023-02-02 09:55:29 -0600

    Numeros alrrevez

---
+++ Semestre2/Java/main3.java
@@ -0,0 +1,16 @@
+// Numeros alrrevez
+import java.util.Scanner;
+
+public class main3 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int a, b, c;
+        a = num.nextInt();
+        b = num.nextInt();
+        c = num.nextInt();
+
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
