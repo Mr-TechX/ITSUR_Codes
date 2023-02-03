a03ec4cd17bd102d20ad28d52e4693cc3c2450b3
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2023-02-03 13:12:52 -0600

    Plantilla java

---
+++ Semestre2/Testing/plantilla.java
@@ -0,0 +1,16 @@
+// Libreria para peticion de datos
+import java.util.Scanner
+// Clase del documento
+public class Main {
+    // Declaracion del main
+    public static void main(String[] args) {
+        // Creaccion del objeto para peticion de datos (en este caso 2num")
+        Scanner num = new Scanner(System.in);
+        // Declaracion de variables enteras
+        int a;
+        // Entrada de la variable
+        a = num.nextInt();
+        // Output
+		    System.out.println(a);
+    }
+}
