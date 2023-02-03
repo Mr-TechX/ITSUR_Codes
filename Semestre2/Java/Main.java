5a5354f8722af3bef46ec4c98fd8bf380def68d7
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2023-02-03 13:07:48 -0600

    Pares con resultado par

---
+++ Semestre2/Java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int a,b;
+	a = num.nextInt();e
+        if(a%2 == 0 && a>2) {
+		System.out.println("SI");
+	} else {
+		System.out.println("NO");
+	}
+    }
+}
