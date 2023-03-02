eddef21004b17f9d94f293019182705d0ecafe2b
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2023-03-02 09:30:11 -0600

    Fibonacci

--- Semestre2/Java/Main7.java
+++ Semestre2/Java/Main7.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Main7 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
