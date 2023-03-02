6f51634740edbe9f7866f64d719751efd1bd685f
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2023-03-02 09:12:16 -0600

    Sucesión de Fibonacci

---
+++ Semestre2/Java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int n1=1;
+        int n2=1;
+        int sum;
+        for(int i=3 ; i<=n ; i++) {
+            sum = n1;
+            n1 = n1 + n2;
+            n2 = sum; 
+        }
+        System.out.println(n1);
+    }
+}
