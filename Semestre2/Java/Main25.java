dace2fcdce4d32774b93f40cea8554f79da0128a
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2023-03-07 20:51:21 -0600

    D. Divisores positivos

---
+++ Semestre2/Java/Main25.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main25 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n, m=0;
+        n = in.nextInt();
+        for(int i = 1;i <= n; i++){
+            if(n%i == 0) {
+                m++;
+            }
+        }
+        System.out.println(m);
+    }
+}
