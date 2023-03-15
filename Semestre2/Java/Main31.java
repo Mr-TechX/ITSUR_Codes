d53ce5263e5a4a41bb750b551eeb15e90927bfba
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2023-03-14 20:56:30 -0600

    Diario de tu hermano

---
+++ Semestre2/Java/Main31.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main31 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String original = sc.nextLine();
+        String reversa = "";
+        for(int i = original.length() - 1; i >= 0; i--) {
+            reversa += original.charAt(i);
+        }
+        System.out.println(reversa);
+    }
+}
