f1af77a47ab9d57196e1ea8ef90a10430ce75cc9
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2023-03-14 20:57:18 -0600

    Planas

---
+++ Semestre2/Java/Main32.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main32 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        int x = in.nextInt();
+
+        for(int i = 0 ; i<x ; i++) {
+            System.out.println(a);
+        }
+    }
+}
