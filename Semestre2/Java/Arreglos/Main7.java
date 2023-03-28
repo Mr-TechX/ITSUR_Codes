1a2ec6037d92840a2eaa73ad76a71f8d1c0d76f9
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2023-03-28 09:47:56 -0600

    Imprimiendo enteros por paridad

---
+++ Semestre2/Java/Arreglos/Main7.java
@@ -0,0 +1,28 @@
+// Librerias
+import java.util.Scanner;
+
+// Code
+public class Main7 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int[] a = new int[1005];
+		int n = in.nextInt();
+        int p;
+		for(int i=0 ; i<=(n-1) ; i++) {
+			a[i] = in.nextInt();
+		}
+        p = in.nextInt();
+		for(int i = 0 ; i<n; i++) {
+			if(p == 1) {
+				if(a[i]%2!=0) {
+					System.out.print(a[i] + " ");
+				}
+			} else if(p == 0) {
+				if(a[i]%2 == 0) {
+					System.out.print(a[i] + " ");
+				}
+			}
+		}
+		
+	}
+}
